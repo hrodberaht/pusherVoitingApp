9af115c493ade1a76050e96dedd7ab4019e15d42
Robert <seedraev@gmail.com>
2018-08-01 21:31:44 +0200

    addin canvas chart, and make voting

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("vote-form");
 
+//Form event
 form.addEventListener("submit", e => {
   const choise = document.querySelector("input[name=os]:checked").value;
   const data = { os: choise };
@@ -17,3 +18,50 @@ form.addEventListener("submit", e => {
 
   e.preventDefault();
 });
+
+let dataPoints = [
+  { label: "Windows", y: 0 },
+  { label: "MacOS", y: 0 },
+  { label: "Linux", y: 0 },
+  { label: "Other", y: 0 }
+];
+
+const chartContainer = document.querySelector("#chartContainer");
+
+if (chartContainer) {
+  const chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "theme1",
+    title: {
+      text: "OS Results"
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: dataPoints
+      }
+    ]
+  });
+  chart.render();
+
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  var pusher = new Pusher("9654d14527f781eda54b", {
+    cluster: "eu",
+    encrypted: true
+  });
+
+  var channel = pusher.subscribe("os-poll");
+  channel.bind("os-vote", function(data) {
+    dataPoints = dataPoints.map(x => {
+      if (x.label == data.os) {
+        x.y += data.points;
+        return x;
+      } else {
+        return x;
+      }
+    });
+    chart.render();
+  });
+}
